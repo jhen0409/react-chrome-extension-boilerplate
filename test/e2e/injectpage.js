7fe329de55cf44c7828320bf2375d5cc87315c92
Jhen <developer@jhen.me>
2015-10-13 11:57:17 +0800

    Use driver.wait wait inject load & run bundle time

--- test/e2e/injectpage.js
+++ test/e2e/injectpage.js
@@ -19,7 +19,7 @@ describe('inject page (in github.com/jhen0409/react-chrome-extension-boilerplate
   });
 
   after(function(done) {
-    this.timeout(15000);
+    this.timeout(20000);
     this.driver.quit().then(done);
   });
 
@@ -31,24 +31,28 @@ describe('inject page (in github.com/jhen0409/react-chrome-extension-boilerplate
   });
 
   it('should render inject app', function(done) {
-    this.driver.findElement(webdriver.By.className('inject-react-example')).then(elem => {
-      done();
-    });
+    // inject load & run bundle time
+    this.timeout(8000);
+    this.driver.wait(() =>
+      this.driver.findElements(webdriver.By.className('inject-react-example'))
+        .then(elems => elems.length > 0)
+    , 5000);
+    done();
   });
 
   it('should link to repo page with click "view repo" link', function(done) {
-    this.timeout(5000);
+    this.timeout(8000);
+    this.driver.wait(() =>
+      this.driver.findElements(webdriver.By.className('inject-react-example-repo-button'))
+        .then(elems => elems.length > 0)
+    , 5000);
     this.driver.findElement(webdriver.By.className('inject-react-example-repo-button')).click();
-    setTimeout(() => {
-      check(done, () => {
-        this.driver.getAllWindowHandles().then(tabs => {
-          this.driver.switchTo().window(tabs[1]);
-          return this.driver.getTitle();
-        }).then(title => {
-          expect(title).to.equal('jhen0409/react-chrome-extension-boilerplate · GitHub');
-          done();
-        });
-      });
-    }, 3000);
+    this.driver.getAllWindowHandles().then(tabs => {
+      this.driver.switchTo().window(tabs[1]);
+      return this.driver.getTitle();
+    }).then((title) => {
+      expect(title).to.equal('jhen0409/react-chrome-extension-boilerplate · GitHub');
+      done();
+    });
   });
 });
