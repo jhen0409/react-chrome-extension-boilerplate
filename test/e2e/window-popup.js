5f9653718548539b4f617e4a9578ff4eee614ea2
Jhen <developer@jhen.me>
2015-10-13 09:55:06 +0800

    Complete window & popup e2e tests

--- test/e2e/window-popup.js
+++ test/e2e/window-popup.js
@@ -2,6 +2,10 @@ import webdriver from 'selenium-webdriver';
 import { expect } from 'chai';
 import { check } from '../func';
 
+function findList(driver) {
+  return driver.findElements(webdriver.By.css('.todo-list > li'));
+}
+
 function addTodo(driver, key) {
   // add todo
   driver.findElement(webdriver.By.className('new-todo')).sendKeys(key + webdriver.Key.RETURN);
@@ -11,7 +15,7 @@ function addTodo(driver, key) {
 }
 
 function editTodo(driver, index, key) {
-  return driver.findElements(webdriver.By.css('.todo-list > li')).then(elems => {
+  return findList(driver).then(elems => {
     // double click label
     const label = elems[index].findElement(webdriver.By.tagName('label'));
     return driver.actions().doubleClick(label).perform();
@@ -25,15 +29,23 @@ function editTodo(driver, index, key) {
 }
 
 function completeTodo(driver, index) {
-  return driver.findElements(webdriver.By.css('.todo-list > li')).then(elems => {
+  return findList(driver).then(elems => {
     // complete todo
     elems[index].findElement(webdriver.By.className('toggle')).click();
     return ({ elem: elems[index], length: elems.length });
   });
 }
 
-function deleteTodo(dirver, index) {
-
+function deleteTodo(driver, index) {
+  return findList(driver).then(elems => {
+    // delete todo
+    driver.executeScript(
+      `document.querySelectorAll('.todo-list > li')[${index}]
+        .getElementsByClassName('destroy')[0].style.display = 'block'`
+    );
+    elems[index].findElement(webdriver.By.className('destroy')).click();
+    return findList(driver);
+  }).then((elems) => ({ length: elems.length }));
 }
 
 describe('window (popup) page', function() {
@@ -101,27 +113,76 @@ describe('window (popup) page', function() {
   });
 
   it('should can complete all todos', function(done) {
-    // TODO
-    done();
+    this.driver.findElement(webdriver.By.className('toggle-all')).click().then(() =>
+      findList(this.driver)
+    ).then((elems) =>
+      Promise.all(elems.map((elem) => elem.getAttribute('class')))
+    ).then(classNames => {
+      expect(classNames.every((name) => name === 'completed')).to.equal(true);
+      done();
+    });
   });
 
   it('should can delete todo', function(done) {
-    // TODO
-    done();
+    deleteTodo(this.driver, 0).then(({ length }) => {
+      expect(length).to.equal(1);
+      done();
+    });
   });
 
-  it('should can clear completed todos', function(done) {
-    // TODO
-    done();
+  it('should can clear completed todos if completed todos count > 0', function(done) {
+    // current list length: 1
+    addTodo(this.driver, 'Add 1').then(() =>
+      addTodo(this.driver, 'Add 2')
+    ).then(({ length }) => {
+      expect(length).to.equal(3);
+      return completeTodo(this.driver, 0);
+    }).then(() => {
+      this.driver.findElement(webdriver.By.className('clear-completed')).click();
+      return findList(this.driver);
+    }).then(elems =>
+      Promise.all(elems.map((elem) => elem.getAttribute('class')))
+    ).then(classNames => {
+      expect(classNames.every((name) => name !== 'completed')).to.equal(true);
+      done();
+    });
+  });
+
+  it('should cannot clear completed todos if completed todos count = 0', function(done) {
+    this.driver.findElements(webdriver.By.className('clear-completed')).then((elems) => {
+      expect(elems.length).to.equal(0);
+      done();
+    });
   });
 
   it('should can filter active todos', function(done) {
-    // TODO
-    done();
+    // current todo count: 2
+    addTodo(this.driver, 'Add 1').then(() =>
+      addTodo(this.driver, 'Add 2')
+    ).then(({ length }) => {
+      expect(length).to.equal(3);
+      return completeTodo(this.driver, 0);
+    }).then(() =>
+      this.driver.findElements(webdriver.By.css('.filters > li'))
+    ).then(elems => {
+      // elems[1] -> Active btn
+      elems[1].click();
+      return findList(this.driver);
+    }).then(elems => {
+      expect(elems.length).to.equal(2);
+      done();
+    });
   });
 
   it('should can filter completed todos', function(done) {
-    // TODO
-    done();
+    // current todo count: 2
+    this.driver.findElements(webdriver.By.css('.filters > li')).then(elems => {
+      // elems[2] -> Completed btn
+      elems[2].click();
+      return findList(this.driver);
+    }).then(elems => {
+      expect(elems.length).to.equal(1);
+      done();
+    });
   });
 });
