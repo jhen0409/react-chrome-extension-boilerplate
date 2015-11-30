d2efeff2821f65b80597c7b6d3da27f855b4f3c1
Jhen <developer@jhen.me>
2015-11-30 20:09:17 +0800

    Fix #13, remove contentBase.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,7 +35,6 @@ gulp.task('replace-webpack-code', () => {
 gulp.task('webpack-dev-server', () => {
   let myConfig = Object.create(devConfig);
   new WebpackDevServer(webpack(myConfig), {
-    contentBase: `https://localhost:${port}`,
     publicPath: myConfig.output.publicPath,
     stats: {colors: true},
     hot: true,
