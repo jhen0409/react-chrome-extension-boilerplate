484c7f3061d00ebf1d7d6c40f851b263b359b9b4
Jhen <developer@jhen.me>
2016-02-12 05:44:23 +0800

    Remove gulp dev, build task, use shelljs instead

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,107 +1,8 @@
 import 'babel-polyfill';
 import fs from 'fs';
 import gulp from 'gulp';
-import merge from 'merge-stream';
-import gulpSync from 'gulp-sync';
-import gutil from 'gulp-util';
-import jade from 'gulp-jade';
-import rename from 'gulp-rename';
 import RSA from 'node-rsa';
 import crx from 'gulp-crx-pack';
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
-import prodConfig from './webpack/prod.config';
-import devConfig from './webpack/dev.config';
-
-const port = 3000;
-const { sync } = gulpSync(gulp);
-
-/*
- * common tasks
- */
-gulp.task('replace-webpack-code', () => {
-  const replaceTasks = [{
-    from: './webpack/replace/JsonpMainTemplate.runtime.js',
-    to: './node_modules/webpack/lib/JsonpMainTemplate.runtime.js'
-  }, {
-    from: './webpack/replace/log-apply-result.js',
-    to: './node_modules/webpack/hot/log-apply-result.js'
-  }];
-  replaceTasks.forEach(task => fs.writeFileSync(task.to, fs.readFileSync(task.from)));
-});
-
-/*
- * dev tasks
- */
-
-gulp.task('webpack-dev-server', () => {
-  let myConfig = Object.create(devConfig);
-  new WebpackDevServer(webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError('webpack', err);
-    gutil.log('Please allow `https://localhost:3000` connections in Google Chrome');
-    gutil.log('and load unpacked extensions with `./dev` folder.  (see https://developer.chrome.com/extensions/getstarted#unpacked)');
-  }), {
-    publicPath: myConfig.output.publicPath,
-    stats: {colors: true},
-    noInfo: true,
-    hot: true,
-    historyApiFallback: true,
-    https: true
-  }).listen(port, 'localhost', (err) => {
-    if (err) throw new gutil.PluginError('webpack-dev-server', err);
-    gutil.log('[webpack-dev-server]', `listening at port ${port}`);
-  });
-});
-
-gulp.task('views:dev', () => {
-  return gulp.src('./chrome/views/*.jade')
-    .pipe(jade({
-      locals: {
-        env: 'dev',
-        devToolsExt: !!process.env.DEVTOOLS_EXT || true
-      }
-    }))
-    .pipe(gulp.dest('./dev'));
-});
-
-gulp.task('copy:dev', () => {
-  const manifest = gulp.src('./chrome/manifest.dev.json')
-    .pipe(rename('manifest.json'))
-    .pipe(gulp.dest('./dev'));
-  const assets = gulp.src('./chrome/assets/**/*').pipe(gulp.dest('./dev'));
-  return merge(manifest, assets);
-});
-
-/*
- * build tasks
- */
-
-gulp.task('webpack:build', callback => {
-  let myConfig = Object.create(prodConfig);
-  webpack(myConfig, (err, stats) => {
-    if (err) {
-      throw new gutil.PluginError('webpack:build', err);
-    }
-    gutil.log('[webpack:build]', stats.toString({ colors: true }));
-    callback();
-  });
-});
-
-gulp.task('views:build', () => {
-  return gulp.src('./chrome/views/*.jade')
-    .pipe(jade({
-      locals: { env: 'prod' }
-    }))
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('copy:build', () => {
-  const manifest = gulp.src('./chrome/manifest.prod.json')
-    .pipe(rename('manifest.json'))
-    .pipe(gulp.dest('./build'));
-  const assets = gulp.src('./chrome/assets/**/*').pipe(gulp.dest('./build'));
-  return merge(manifest, assets);
-});
 
 /*
  * compres tasks
@@ -126,6 +27,4 @@ gulp.task('crx:compress', () => {
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', ['replace-webpack-code', 'webpack-dev-server', 'views:dev', 'copy:dev']);
-gulp.task('build', ['replace-webpack-code', 'webpack:build', 'views:build', 'copy:build']);
-gulp.task('compress', sync(['build', 'crx:compress']));
+gulp.task('compress', 'crx:compress');
---
+++ scripts/tasks.js
@@ -0,0 +1,23 @@
+require('shelljs/global');
+const fs = require('fs');
+
+exports.replaceWebpack = () => {
+  const replaceTasks = [{
+    from: 'webpack/replace/JsonpMainTemplate.runtime.js',
+    to: 'node_modules/webpack/lib/JsonpMainTemplate.runtime.js'
+  }, {
+    from: 'webpack/replace/log-apply-result.js',
+    to: 'node_modules/webpack/hot/log-apply-result.js'
+  }];
+
+  replaceTasks.forEach(task => cp(task.from, task.to));
+}
+
+exports.copyAssets = type => {
+  const env = type === 'build' ? 'prod' : type;
+  rm('-rf', type);
+  mkdir(type);
+  cp(`chrome/manifest.${env}.json`, type + '/manifest.json');
+  cp('-R', 'chrome/assets/', type);
+  exec(`jade -O "{ env: '${env}' }" -o ${type} chrome/views/*.jade`);
+}
