2397dc30590f75f75e678ea6fdc8dfe684a6398a
Jhen <developer@jhen.me>
2016-11-13 17:35:24 +0800

    Fix extension compose usage, fixes #54
    if missing object it will be `compose` function

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -6,9 +6,9 @@ import storage from '../utils/storage';
 // If Redux DevTools Extension is installed use it, otherwise use Redux compose
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
     // Options: http://zalmoxisus.github.io/redux-devtools-extension/API/Arguments.html
-  ) :
+  }) :
   compose;
 /* eslint-enable no-underscore-dangle */
 
