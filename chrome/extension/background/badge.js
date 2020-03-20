4ea3402f7b1a4370b3c8f7aabf8cc9f1ede99f6e
bekusib <jeenhyung@users.noreply.github.com>
2020-03-21 02:50:06 +0900

    fixed badge.js

--- chrome/extension/background/badge.js
+++ chrome/extension/background/badge.js
@@ -1,7 +1,7 @@
-chrome.storage.local.get('todos', (obj) => {
-  let todos = obj.todos;
+chrome.storage.local.get('state', (obj) => {
+  const state = JSON.parse(obj.state);
+  const todos = state.todos;
   if (todos) {
-    todos = JSON.parse(todos);
     const len = todos.filter(todo => !todo.marked).length;
     if (len > 0) {
       chrome.browserAction.setBadgeText({ text: len.toString() });
